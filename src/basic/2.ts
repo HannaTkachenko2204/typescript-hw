96f1aad4a5c4239a31b90e6a7a46ae12726e79f4
Анна <anna.kuvakina91@gmail.com>
2024-07-13 16:54:41 +0300

    basic-tasks

---
+++ src/basic/2.ts
@@ -0,0 +1,4 @@
+// let person = ['Max', 21];
+
+// концепція кортежів
+let person: [string, number] = ['Max', 21];
---
+++ src/basic/3.ts
@@ -0,0 +1,5 @@
+// або рядок, або число (union type)
+let value: string | number;
+
+// лише 'enable' або 'disable' (literal type)
+let userStatus: "enable" | "disable";
